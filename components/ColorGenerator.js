4132f741c77e8ca36fa524de197c6187f5062be5
BilalMughal3993 <98820231+BilalMughal3993@users.noreply.github.com>
2022-03-01 10:31:05 +0500

    create components folder and colorGenerator.js file

--- components/ColorGenerator.js
+++ components/ColorGenerator.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import {View,Text,TouchableOpacity,StyleSheet} from 'react-native'
+import {View,Text,TouchableOpacity,StyleSheet,Alert} from 'react-native'
+
 
 const ColorGenerator = () => {
+    const randomColor = ()=>{
+        const red =Math.floor( Math.random()*256);
+        const blue =Math.floor( Math.random()*256);
+        const green =Math.floor( Math.random()*256);
+        return`rbg(${red},${blue},${green})`
+        
+    }
   return (
 
     <View >
         <TouchableOpacity
-        // onPress={codeGenerator}
+        onPress={()=>{
+            Alert.alert(randomColor())}
+        }
          style={styles.containerC}>
         <Text style={styles.text1}>Random color Generator</Text>
         </TouchableOpacity>
+        <View style={styles.containerC2}>
+            <Text style={styles.text2}>
+                
+            </Text>
+        </View>
 
     </View>
   )
@@ -17,12 +32,21 @@ const ColorGenerator = () => {
 const styles =StyleSheet.create({
     containerC:{
         marginTop:50,
-      
         backgroundColor:"grey"
     },
     text1:{
 fontSize:30,
 padding:10
+    },
+    containerC2:{
+      
+        marginTop:60,
+        
+        
+    },
+    text2:{
+        padding:20,
+        backgroundColor:'grey'
     }
 
 })
